f92724b849436aa3f4d8ece8b2a44628d6b28be6
Glen Carpenter <glen@glencarpenter.io>
2019-03-21 11:57:04 -0600

    Updates

---
+++ precache-manifest.395a763aea9445e1b52e693645760646.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "38935d5adf3386a29fcf",
+    "url": "/poetry-simulator/static/js/runtime~main.7b3d5327.js"
+  },
+  {
+    "revision": "89ed917df4bb2c83689a",
+    "url": "/poetry-simulator/static/js/main.3f488736.chunk.js"
+  },
+  {
+    "revision": "a35691f01c9d5a80e794",
+    "url": "/poetry-simulator/static/js/2.9ec8c501.chunk.js"
+  },
+  {
+    "revision": "89ed917df4bb2c83689a",
+    "url": "/poetry-simulator/static/css/main.33c7407b.chunk.css"
+  },
+  {
+    "revision": "073c6f63426197cd10812b43b98e917b",
+    "url": "/poetry-simulator/index.html"
+  }
+];
